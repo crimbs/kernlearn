aa82c61e938b7200d7e2dece0fae15665226c2e2
Hines, Christian L <christian.hines20@imperial.ac.uk>
2022-08-04 20:38:18 +0100

    Added pytest

--- tests.py
+++ tests.py
@@ -1,22 +1,30 @@
-from kernlearn import utils
-
-# from kernlearn.nn_models import *
-
-from scipy.spatial.distance import pdist, squareform
-import numpy as np
+import pytest
 import jax.numpy as jnp
 from jax import random
-from jax import config
+from scipy.spatial import KDTree
+from scipy.spatial.distance import pdist, squareform
 
-config.update("jax_enable_x64", True)
+from kernlearn import utils
+
+rng = random.PRNGKey(0)
 
 
-def test_pdist():
-    seed = 0
-    rng = random.PRNGKey(seed)
+@pytest.mark.parametrize("N", [10, 50])
+def test_pdist(N):
     N = 10
     d = 2
     x = random.uniform(rng, (N, d))
-    scipy_pdist = squareform(pdist(np.asarray(x)))
-    utils_pdist = utils.pdist(utils.pdisp(x, x))
+    scipy_pdist = squareform(pdist(x))
+    utils_pdist = utils.pdist(x, x)
     assert jnp.allclose(scipy_pdist, utils_pdist)
+
+
+@pytest.mark.parametrize("N", [10, 50])
+def test_nearest_neighbours(N):
+    d = 2
+    k = N // 2
+    x = random.uniform(rng, (N, d))
+    scipy_dists, scipy_inds = KDTree(x).query(x, k)
+    utils_dists, utils_inds = utils.nearest_neighbours(utils.pdist(x, x), k)
+    assert jnp.allclose(scipy_dists, utils_dists)
+    assert jnp.allclose(scipy_inds, utils_inds)
